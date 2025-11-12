5a5c8e2472b62e3ff7e0dce12a487e1bcbe19e9c
Pedro Colantuano Lima <48564809+pedrocolantuano@users.noreply.github.com>
2025-11-12 10:38:01 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// Arquivo principal
+import Header from "./components/Header"
+function App(){
+  return(
+    <>
+    <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+function Header(){
+    return(
+        <header>
+            <img src="" alt="logo marca da pagina" />
+            <ul>
+                <li>LOOKS</li>
+                <li>LANÇAMENTOS</li>
+                <li>NOVIDADES</li>
+            </ul>
+
+        </header>
+    )
+}
+
+export default Header
